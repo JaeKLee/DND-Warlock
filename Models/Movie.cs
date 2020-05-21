c2b0dc01064b8a8d155a68cc21647ad975762851
JaeKLee <jae.lee2@marist.edu>
2020-05-21 16:53:19 -0700

    Changed names being displayed
    Changed ReleaseDate to Release Date and set data type for price to match currency

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DND_Warlock.Models
 {
@@ -8,9 +9,12 @@ namespace DND_Warlock.Models
         public int ID { get; set; } // Primary key
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        
+        [Column(TypeName = "decimal(18, 2")]
         public decimal Price { get; set; }
     }
 }
